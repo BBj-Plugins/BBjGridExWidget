1d0e1de27712ba017c5045765c72cda1c3e5c7f0
Hyyan Abo Fakher <hyyanaf@gmail.com>
2017-09-27 16:01:46 +0200

    Add basic date format function

--- js/BBjGridExWidget.js
+++ js/BBjGridExWidget.js
@@ -6,12 +6,99 @@
 * file that was distributed with this source code.
 */
 
+bbj_grid_supported_value_formatter = {
+  'date': bbj_grid_widget_get_value_formatter_date
+}
+
+function bbj_formate_date(date, format) {
+
+  if (!date || !format) return null;
+
+  date = new Date(date);
+
+  var year = date.getYear();
+  var fullYear = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day = date.getDay();
+  var hours24 = date.getHours();
+  var hours12 = hours24 - 12;
+  hours12 = hours12 <= 12 ? hours12 : hours24;
+  hours12 = hours12 == 0 ? 12 : hours12;
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  var translations = {
+
+    // year 
+    "Yz": year,
+    "Ys": fullYear,
+    "Yl": fullYear,
+    "Yp": String.fromCharCode(year),
+    "Yd": fullYear,
+    "Y": fullYear,
+
+    // month
+    "Mz": String(month).length == 1 ? "0" + month : month,
+    "Ms": month,
+    "Ml": month,
+    "Mp": String.fromCharCode(month),
+    "Md": month,
+    "M": month,
+
+    // day
+    "Dz": String(day).length == 1 ? "0" + day : day,
+    "Ds": day,
+    "Dl": day,
+    "Dp": String.fromCharCode(day),
+    "Dd": day,
+    "D": day,
+
+    // hour 24
+    "Hs": hours24,
+    "Hl": hours24,
+    "Hp": String.fromCharCode(hours24),
+    "Hd": hours24,
+    "H": hours24,
+
+    // hour 12
+    "hs": String(hours12).length == 1 ? "0" + hours12 : hours12,
+    "hl": hours12,
+    "hp": String.fromCharCode(hours12),
+    "hd": hours12,
+    "h": hours12,
+
+    // minutes
+    "ms": String(minutes).length == 1 ? "0" + minutes : minutes,
+    "ml": minutes,
+    "mp": String.fromCharCode(minutes),
+    "md": minutes,
+    "m": minutes,
+
+    // seconds
+    "ss": String(seconds).length == 1 ? "0" + seconds : seconds,
+    "sl": seconds,
+    "sp": String.fromCharCode(seconds),
+    "sd": seconds,
+    "s": seconds,
+
+    'pd': hours24 > 12 ? 'PM' : 'AM',
+    'p': hours24 > 12 ? 'PM' : 'AM',
+  };
+
+  var result = format;
+  for (var k in translations) {
+    result = result.replace(new RegExp('(%' + k + ')', 'g'), translations[k]);
+  }
+
+  return result;
+}
+
 function bbj_grid_widget_post_event(ev) {
   window.basisDispatchNativeEvent(ev);
 }
 
 function bbj_grid_widget_send_event(payload) {
-	
+
   console.log(payload);
   var d = $doc.getElementById('eventTransporterDiv');
   var event = new Event('click');
@@ -112,6 +199,9 @@ function bbj_grid_widget_init(container, license, data, options) {
 
   for (var i in options.columnDefs) {
     options.columnDefs[i].cellStyle = bbj_grid_widget_cell_render;
+    options.columnDefs[i].valueFormatter = bbj_grid_widget_get_value_formatter(
+      options.columnDefs[i].filter
+    );
   }
 
   return new agGrid.Grid(container, options);
@@ -122,12 +212,27 @@ function bbj_grid_widget_fit_grid(fitmode) {
 }
 
 function bbj_grid_widget_get_state() {
+
   var state = $doc.bbj_grid_widget.columnApi.getColumnState();
   return JSON.stringify(state);
 }
 
 function bbj_grid_widget_set_state(state) {
   $doc.bbj_grid_widget.columnApi.setColumnState(state);
+}
+
+function bbj_grid_widget_get_value_formatter(filter) {
+
+  return bbj_grid_supported_value_formatter.hasOwnProperty(filter) ?
+    bbj_grid_supported_value_formatter[filter] : null;
+}
+
+function bbj_grid_widget_get_value_formatter_date(data) {
+
+  return bbj_formate_date(
+    data.value,
+    '%Y-%Mz-%Dz',
+  );
 }
 
 function bbj_grid_widget_set_data(json, options) {
