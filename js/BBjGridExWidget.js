359cd7b641a90130ebf0f2985663c196c8f1cd04
Hyyan Abo Fakher <hyyanaf@gmail.com>
2018-01-17 15:52:10 +0100

    Resolve issue #8 - Add 'setVisibleColumn' method

--- js/BBjGridExWidget.js
+++ js/BBjGridExWidget.js
@@ -172,6 +172,10 @@ function bbj_grid_widget_set_visible_row(index,position){
   $doc.bbj_grid_widget.api.ensureIndexVisible(index,position);
 }
 
+function bbj_grid_widget_set_visible_column(columnId) {
+  $doc.bbj_grid_widget.api.ensureColumnVisible(columnId);
+}
+
 function bbj_grid_widget_get_state() {
 
   var state = $doc.bbj_grid_widget.columnApi.getColumnState();
