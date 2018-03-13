0bcf8132c4fe79c623c05989aa8cf4c05b79fa9e
Hyyan Abo Fakher <hyyanaf@gmail.com>
2018-03-13 13:58:32 +0100

    Upgrade to ag-grid-components.git#v1.5.2

--- js/src/init.js
+++ js/src/init.js
@@ -195,7 +195,6 @@ export function gw_setData(json, options, license) {
   const container = $doc.getElementById('grid');
   container.innerHTML = '';
 
-  console.log(options)
   window.gw_meta = json[0].meta;
   window.AGridComponentsMetaConfig = gw_meta;
 
