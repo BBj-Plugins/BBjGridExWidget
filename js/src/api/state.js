fa8553be57051ed33d953a4cea14b6fae037bae6
Hyyan Abo Fakher <hyyanaf@gmail.com>
2018-12-12 19:12:18 +0100

    Improve the grid state

--- js/src/api/state.js
+++ js/src/api/state.js
@@ -1,13 +1,37 @@
+/*
+* This file is part of the grid project
+* (c) Basis Europe <eu@basis.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 export function gw_setState(state) {
-  gw_options.columnApi.setColumnState(state);
+  try {
+    gw_options.columnApi.setColumnState(state.columns);
+    gw_options.columnApi.setColumnGroupState(state.groups);
+    gw_options.api.setSortModel(state.sort);
+    gw_options.api.setFilterModel(state.filters);
+  } catch (e) {
+    console.warn('Failed to parse grid state from JSON', e);
+  }
 }
 
 export function gw_getState() {
 
-  const state = gw_options.columnApi.getColumnState();
+  const columns = gw_options.columnApi.getColumnState();
+  const groups = gw_options.columnApi.getColumnGroupState();
+  const sort = gw_options.api.getSortModel();
+  const filters = gw_options.api.getFilterModel();
+
   try {
-    return JSON.stringify(state);
+    return JSON.stringify({
+      columns,
+      groups,
+      sort,
+      filters
+    });
   } catch (e) {
-    console.warn('Failed to parse state', e);
+    console.warn('Failed to convert the grid state to json', e);
   }
 }
