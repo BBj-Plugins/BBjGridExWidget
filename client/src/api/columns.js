9998da835af20bf36397f67a6657450514a46cc3
Hyyan Abo Fakher <hyyanaf@gmail.com>
2019-05-12 16:39:51 +0200

    (feat) Improve selection api (#134)
    * add `setNavigationBehavior`
    * add `setShowHeaderSelectionCheckbox`
    * add `setRowMultiSelectWithClick`
    * add `setRowDeselection`
    * add `setSuppressRowClickSelection`
    * remove `setSelectionMode`
    * Selection checkbox is now always rendered on the first column even after columns re-order
    * Build sophisticated selection demo to show selections features
    
    resolves #104

--- client/src/api/columns.js
+++ client/src/api/columns.js
@@ -62,3 +62,47 @@ export function gw_groupColumns(columns, columnDefs) {
     columnDefs.unshift(column);
   }
 }
+
+/**
+ * Show / Hide selection checkbox based on the first column
+ * 
+ * The function make sure that the selection checkbox is always shown on the 
+ * first column
+ * 
+ * @param {Object} param 
+ * 
+ * @return {Boolean} true when first column , false if not the first column or if the 
+ *                   grid's `Context.showSelectionCheckbox` is false
+ */
+export function gw_isShowSelectionCheckbox(param) {
+
+  if(!param.context.showSelectionCheckbox) return false;
+
+  const columns = param.columnApi.getAllDisplayedVirtualColumns();
+
+  return columns[0].colDef.field === param.colDef.field
+}
+
+/**
+ * Show / Hide header selection checkbox based on the first column
+ * 
+ * The function make sure that the header selection checkbox is always shown on the 
+ * first column
+ * 
+ * @param {Object} param 
+ * 
+ * @return {Boolean} true when first column , false if not the first column or if the 
+ *                   grid's `Context.showHeaderSelectionCheckbox` is false
+ */
+export function gw_isHeaderCheckboxSelection(param) {
+
+  const context = param.api.gridOptionsWrapper.gridOptions.context;
+
+  if(!context.showHeaderSelectionCheckbox) return false;
+
+  const columns = param.columnApi.getAllDisplayedVirtualColumns();
+
+  return columns[0].colDef.field === param.colDef.field
+}
+
+
