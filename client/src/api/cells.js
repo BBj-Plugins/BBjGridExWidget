347244cc0f984f5d6c2460556d16ab34c02e1a61
Hyyan Abo Fakher <hyyanaf@gmail.com>
2019-07-19 15:59:00 +0200

    chore(JS): move range functions to the cells module

--- client/src/api/cells.js
+++ client/src/api/cells.js
@@ -72,3 +72,89 @@ export function gw_setFocusedCell(id, row, column) {
     .api
     .setFocusedCell(r, c);
 }
+
+/**
+ * Get Range Selections
+ * 
+ * The method will create a range json model for each range which can be parsed in BBj side.
+ * 
+ * @param {Number} id grid's id
+ * 
+ * @returns {String} selected ranges as JSON
+ */
+export function gw_getRangeSelections(id) {
+  const options = gw_getGrid(id).options;
+  const context = options.context;
+  const api = options.api;
+  const ranges = api.getCellRanges();
+  const model = api.getModel();
+  let result = [];
+
+  ranges.forEach(range => {
+
+    // const start = gw_parseNode(model.getRow(range.startRow.rowIndex), options.context);
+    // const end = gw_parseNode(model.getRow(range.endRow.rowIndex), options.context);
+
+    //if (start !== false && end !== false) {
+    const columns = range.columns.reduce((accumulator, current) => {
+
+      if ("ag-Grid-AutoColumn" !== current.colId) {
+        accumulator.push(current.colId);
+      }
+
+      return accumulator;
+    }, []).filter(Boolean);
+
+    const starIndex = Math.min(range.startRow.rowIndex, range.endRow.rowIndex);
+    const endIndex = Math.max(range.startRow.rowIndex, range.endRow.rowIndex);
+    const rows = [];
+
+    for (let rowIndex = starIndex; rowIndex <= endIndex; rowIndex++) {
+      const node = model.getRow(rowIndex);
+      if (node) {
+        const parsedNode = gw_parseNode(node, context);
+        if (parsedNode) rows.push(parsedNode);
+      }
+    }
+
+    if (rows.length && columns.length) {
+      result.push({ r: rows, c: columns });
+    }
+    //}
+  });
+
+  return JSON.stringify(result);
+}
+
+/**
+ * Add new cell range
+ * 
+ * @param {Number} id grid's id
+ * @param {Object} range  bounded or unbounded range model
+ */
+export function gw_addCellRange(id, range) {
+  const options = gw_getGrid(id).options;
+  const pr = deepParseJson(JSON.stringify(range));
+  const start = !(pr.start || null) ?
+    0 : (Number.isInteger(+pr.start) ? +pr.start : options.api.getRowNode(pr.start).rowIndex);
+  const end = !(pr.end || null) ?
+    (options.rowData.length - 1) : (Number.isInteger(+pr.end) ? +pr.end : options.api.getRowNode(pr.end).rowIndex);
+
+  options.api.addCellRange({
+    rowStartIndex: Math.abs(start),
+    rowEndIndex: Math.abs(end),
+    columns: pr.columns
+  });
+}
+
+/**
+ * Clears the selected range.
+ * 
+ * @param {Number} id grid's id
+ */
+export function gw_clearRangeSelection(id) {
+  gw_getGrid(id)
+    .options
+    .api
+    .clearRangeSelection();
+}
