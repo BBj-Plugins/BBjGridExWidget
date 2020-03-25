d1608821c29827417b24c3f2d63315b109a2fa34
Hyyan Abo Fakher <hyyanaf@gmail.com>
2020-03-25 12:39:27 +0100

    feat(JS): add custom tooltip component

--- client/src/components/HTMLTooltip.js
+++ client/src/components/HTMLTooltip.js
@@ -7,7 +7,7 @@
  */
 
 /**
- * An improved tooltip component which supports HTML and works behaves the same in both GUI and BUI
+ * An improved tooltip component which supports HTML and behaves the same in both GUI and BUI
  *
  * @author Hyyan Abo Fakher
  */
@@ -19,25 +19,39 @@ class HTMLTooltip {
    */
   init(params) {
     const eGui = (this.eGui = document.createElement('div'))
-    const tooltipField = params.colDef.tooltipField
     const tooltipValueGetter = params.colDef.tooltipValueGetter
+    const isHeader = params.rowIndex === undefined
+    const isGroupedHeader = isHeader && !!params.colDef.children
+    // eslint-disable-next-line no-prototype-builtins
+    const data =
+      !isHeader && !isGroupedHeader
+        ? params.api.getDisplayedRowAtIndex(params.rowIndex).data
+        : null
     const theme = params.api.gridCore.eGridDiv.className.endsWith('dark')
       ? 'gw-tooltip-dark'
       : 'gw-tooltip-light'
-    console.log(
-      theme,
-      params.api.gridCore.eGridDiv.className,
-      params.api.gridCore.eGridDiv.className.endsWith('dark')
-    )
+    const tooltipField = params.colDef.tooltipField
+      ? // eslint-disable-next-line no-prototype-builtins
+        data && data.hasOwnProperty(params.colDef.tooltipField)
+        ? data[params.colDef.tooltipField]
+        : null
+      : null
+
+    const passedParams = {
+      ...params,
+      ...{ data },
+      ...{ isHeader, isGroupedHeader, tooltipField },
+    }
+
     eGui.classList.add('gw-tooltip', theme)
-    eGui.innerHTML = tooltipField
-      ? tooltipField
-      : tooltipValueGetter
-      ? tooltipValueGetter({
-          ...params,
-          ...{ data: params.api.getDisplayedRowAtIndex(params.rowIndex).data },
-        })
-      : ''
+    eGui.innerHTML =
+      isHeader || isGroupedHeader
+        ? params.value
+        : tooltipField
+        ? tooltipField
+        : tooltipValueGetter
+        ? tooltipValueGetter(passedParams)
+        : params.value
   }
 
   /**
