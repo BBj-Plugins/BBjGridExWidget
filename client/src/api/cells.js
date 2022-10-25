cc3092a9681b68a530c646c5219301e030ce6106
Hyyan Abo Fakher <hyyanaf@gmail.com>
2022-10-25 11:26:38 +0200

    fix: :bug: Grid fails to focus and start editing a column if its empty closes #245

--- client/src/api/cells.js
+++ client/src/api/cells.js
@@ -34,8 +34,10 @@ export function gw_startEditingCell(
   const api = options.api
   const node = api.getRowNode(row) || api.getDisplayedRowAtIndex(row)
 
+  console.log(row, node)
+
   options.api.startEditingCell({
-    rowIndex: node.rowIndex,
+    rowIndex: node ? node.rowIndex : row,
     keyPress: Number(keyPress),
     colKey,
     charPress,
